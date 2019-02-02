2b59ef2fb062ff9dbba89818e77f956ffd9f07b9
Phoenix <theemaraldblock@gmail.com>
2019-02-02 11:52:32 +0000

    Update Assistant.js

--- Assistant.js
+++ Assistant.js
@@ -1,4 +1,35 @@
 const Discord = require('discord.js')
 const assis = new Discord.Client() 
+var prefix = (">")
+
+//PreSense Assistant Security//
+assis.on('message', message => {
+    if (message.content.startsWith(prefix + "eval process.env.NONEOFYOURBUISNESS")) {
+	    message.channel.send("**System:** Access Denied ")
+sleep.sleep(3)
+    message.channel.send("Lockdown Sequence Started")
+	  assis.user.setActivity("Shutdown Sequence Initated | Contact Owners Now.") 
+	   assis.user.setStatus("dnd")
+	    message.channel.send("Shutdown Sequence has been Initated Bye Bye!")
+	    process.exit();
+    }
+});
+
+assis.on('message', message => {
+    if (message.content.startsWith(prefix + "eval assis.token")) {
+	    message.channel.send("**System:** Access Denied ")
+sleep.sleep(3)
+    message.channel.send("Lockdown Sequence Started")
+	  assis.user.setActivity("Shutdown Sequence Initated | Contact Owners Now.") 
+	   assis.user.setStatus("dnd")
+	    message.channel.send("Shutdown Sequence has been Initated Bye Bye!")
+	    process.exit();
+    }
+});
+
+
+///
+
+
 
 assis.login(process.env.NONEOFYOURBUISNESS)
