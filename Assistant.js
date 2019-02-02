194ceb4fe62c6e59f6fb9d58cd90c57e0ac8fd86
Phoenix <theemaraldblock@gmail.com>
2019-02-02 12:05:01 +0000

    Update Assistant.js

--- Assistant.js
+++ Assistant.js
@@ -27,7 +27,89 @@ assis.on('message', message => {
 });
 
 
-///
+/////////////////////////
+
+
+
+
+//Eval///
+Assistant.on('message', message => {
+	if (message.author.id === "498186211249225728") {
+		let msg = message.content;
+
+		if (msg.startsWith(">eval")) {
+   		msg = msg.substring(">eval ".length)
+   		msg = msg.replace(/```js/gi, "")
+   		msg = msg.replace(/```/gi, "")
+
+   		try {
+   			let result = eval(msg)
+
+    		if (typeof(result) == "undefined") {
+    			message.channel.send({
+    				embed: {
+    					description: "This code didn't return any value",
+    					fields: [
+    						{
+    							name: ":inbox_tray: Input",
+    							value: "```" + msg + "```"
+    						},
+    						{
+    							name: ":outbox_tray: Output",
+    							value: "Nothing here :innocent:"
+    						}
+  	  				],
+  	  				footer: {
+  	  					text: "PhoenixEval"
+  	  				}
+  	  			}
+  	  		})
+  	  	} else {
+  	  		message.channel.send({
+  	  			embed: {
+    					fields: [
+    						{
+    							name: ":inbox_tray: Input",
+    							value: "```" + msg + "```"
+    						},
+    						{
+	    						name: ":outbox_tray: Output",
+  	  						value: "```" + result + "```"
+    						}
+    					],
+    					footer: {
+    						text: "PhoenixEval"
+    					}
+	    			}
+  	  		})
+    		}
+   		} catch (e) {
+   			message.channel.send({
+  	  			embed: {
+  	  				description: ":dizzy_face: Something went wrong",
+    					fields: [
+    						{
+    							name: ":inbox_tray: Input",
+    							value: "```" + msg + "```"
+    						},
+    						{
+    							name: "Error content",
+    							value: "```" + e + "```"
+    						}
+    					],
+    					footer: {
+    						text: "PhoenixEval"
+    					}
+	    			}
+  	  		})
+   		}
+  	}
+	}
+})
+
+
+
+/////
 
 
 
